abc23f3345c824ba632afcc855b813598574470d
vishalkondle-dev <vishalkondle.dev@gmail.com>
2025-11-03 23:42:44 +0530

    setup: constants package used: in utils package

--- packages/utils/date.ts
+++ packages/utils/date.ts
@@ -1,8 +1,9 @@
 import dayjs from "dayjs";
+import { DATE_FORMAT } from "@vishal/constants";
 
 export const formatDate = (
   date: Date,
-  format: string = "YYYY-MM-DD"
+  format: string = DATE_FORMAT
 ): string => {
   return dayjs(date).format(format);
 };
