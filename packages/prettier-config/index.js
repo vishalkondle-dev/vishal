aa09198aafad0bfc12d243980d93b7cd168612e4
vishalkondle-dev <vishalkondle.dev@gmail.com>
2025-11-03 23:27:08 +0530

    setup: prettier-config package

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
